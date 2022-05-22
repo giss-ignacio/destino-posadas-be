a38c2fcf25adc30c24909aa2ca0a80014467e361
Gustavo Caradonna <gustavo@datawebhosting.com.ar>
2022-05-22 16:19:24 -0300

    primeros cambios en booking.js

--- routes/booking.js
+++ routes/booking.js
@@ -4,6 +4,9 @@ const router = express.Router();
 
 //GET ID HOTELES
 router.get("/hoteles", async function (req, res, next) {
+  let respuesta;
+  let respuesta2 = [];
+  //endpoint de lista de hoteles
   const options = {
     method: "GET",
     url: "https://travel-advisor.p.rapidapi.com/hotels/list",
@@ -22,7 +25,6 @@ router.get("/hoteles", async function (req, res, next) {
       "X-RapidAPI-Key": "9d38a8bc8cmsh161083924f024ffp19b2bajsn5cb83b18933b",
     },
   };
-  let respuesta;
 
   await axios
     .request(options)
@@ -33,7 +35,60 @@ router.get("/hoteles", async function (req, res, next) {
       console.error(error);
     });
 
-  res.json(respuesta);
+  //Mi test
+  //respuesta.data es el array de hoteles q tiene el objeto respuesta
+
+  let arrayDeObjetos = respuesta.data;
+  let arrayIds = [];
+
+  await arrayDeObjetos.map((hotel) => {
+    arrayIds.push(hotel.location_id);
+  });
+
+  await console.log(arrayIds);
+
+  // map de mi array de ids
+  let destVARIABLE;
+
+  await arrayIds.map((idHotel) => {
+    //  dame tal cosa por idHotel
+    destVARIABLE = idHotel;
+
+    const options2 = {
+      method: "GET",
+      url: "https://booking-com.p.rapidapi.com/v1/hotels/search",
+      params: {
+        checkout_date: "2022-10-01",
+        units: "metric",
+        dest_id: destVARIABLE,
+        dest_type: "city",
+        locale: "en-gb",
+        adults_number: "1",
+        order_by: "popularity",
+        filter_by_currency: "AED",
+        checkin_date: "2022-09-30",
+        room_number: "1",
+      },
+      headers: {
+        "X-RapidAPI-Host": "booking-com.p.rapidapi.com",
+        "X-RapidAPI-Key": "9d38a8bc8cmsh161083924f024ffp19b2bajsn5cb83b18933b",
+      },
+    };
+
+    axios
+      .request(options2)
+      .then(function (response2) {
+        console.log(response2.data);
+        respuesta2.push(response2.data);
+      })
+      .catch(function (error2) {
+        console.error(error2);
+      });
+  });
+
+  await console.log("la respuesta es: " + respuesta2);
+  //////////////////////////////////////
+  res.json(respuesta2);
 });
 
 module.exports = router;
