8ef3cbc098c181f6794ded6f5270d36b23f36846
Gustavo Caradonna <gustavo@datawebhosting.com.ar>
2022-05-26 19:44:53 -0300

    ajuste

--- routes/booking.js
+++ routes/booking.js
@@ -5,7 +5,7 @@ const router = express.Router();
 //GET HOTELES
 let arrHotelesConInfo = [];
 
-router.get("/hoteles", function (req, res, next) {
+router.get("/hoteles", async function (req, res, next) {
   let arrayIdsMock = [];
   //"8051135", "5931564"
 
@@ -32,10 +32,11 @@ router.get("/hoteles", function (req, res, next) {
     },
   };
 
-  axios
+  await axios
     .request(options)
     .then(function (response) {
       listaDeHoteles = response.data;
+      console.log(listaDeHoteles);
     })
     .catch(function (error) {
       console.error(error);
@@ -52,27 +53,27 @@ router.get("/hoteles", function (req, res, next) {
 
   // QUERY2
 
-  arrayIdsMock.forEach((id) => {
-    const options = {
-      method: "GET",
-      url: "https://booking-com.p.rapidapi.com/v1/hotels/review-scores",
-      params: { locale: "en-gb", hotel_id: id },
-      headers: {
-        "X-RapidAPI-Host": "booking-com.p.rapidapi.com",
-        "X-RapidAPI-Key": "9d38a8bc8cmsh161083924f024ffp19b2bajsn5cb83b18933b",
-      },
-    };
+  // arrayIdsMock.forEach((id) => {
+  //   const options = {
+  //     method: "GET",
+  //     url: "https://booking-com.p.rapidapi.com/v1/hotels/review-scores",
+  //     params: { locale: "en-gb", hotel_id: id },
+  //     headers: {
+  //       "X-RapidAPI-Host": "booking-com.p.rapidapi.com",
+  //       "X-RapidAPI-Key": "9d38a8bc8cmsh161083924f024ffp19b2bajsn5cb83b18933b",
+  //     },
+  //   };
 
-    axios
-      .request(options)
-      .then(function (response) {
-        // console.log(response.data);
-        arrHotelesConInfo.push(response.data);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  });
+  //   axios
+  //     .request(options)
+  //     .then(function (response) {
+  //       // console.log(response.data);
+  //       arrHotelesConInfo.push(response.data);
+  //     })
+  //     .catch(function (error) {
+  //       console.error(error);
+  //     });
+  // });
   res.json(arrHotelesConInfo);
 });
 
