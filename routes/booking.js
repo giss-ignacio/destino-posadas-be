99974745320956119a1ec63532f6c72f38da82e5
Gustavo Caradonna <gustavo@datawebhosting.com.ar>
2022-05-29 17:46:12 -0300

    Terminada la api de /hoteles

--- routes/booking.js
+++ routes/booking.js
@@ -4,7 +4,6 @@ const router = express.Router();
 
 //GET HOTELES
 
-
 async function consultaDeHoteles() {
   // endpoint de lista de hoteles
   const options = {
@@ -34,7 +33,6 @@ async function consultaDeHoteles() {
   await axios
     .request(options)
     .then(function (response) {
-      
       listaDeHoteles.push(response.data);
     })
     .catch(function (error) {
@@ -45,67 +43,61 @@ async function consultaDeHoteles() {
   ////// GET IDS HOTELES /////////////
   ////////////////////////////////////
 
-
   return listaDeHoteles[0].result.map((e) => {
     return e.hotel_id;
-  })
+  });
 }
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+function sleep(ms, work) {
+  return new Promise((resolve) => setTimeout(resolve(work), ms));
 }
 
 async function consultaUnHotel(id) {
   try {
-     let res = await axios({
-          url: 'https://booking-com.p.rapidapi.com/v1/hotels/review-scores',
-          method: 'get',
-          params: { locale: "en-gb", hotel_id: id },
-          timeout: 8000,
-          headers: {
-              'X-RapidAPI-Host': 'booking-com.p.rapidapi.com',
-              'X-RapidAPI-Key': 'd1c9714594msh22b6c163e2c4be6p129e59jsnc056ede1f158'
-          }
-      })
-      if(res.status == 200){
-          // test for status you want, etc
-          console.log(res.status)
-      }    
-      // Don't forget to return something   
-      return res.data
-  }
-  catch (err) {
-      console.error(err);
+    let res = await axios({
+      url: "https://booking-com.p.rapidapi.com/v1/hotels/review-scores",
+      method: "get",
+      params: { locale: "en-gb", hotel_id: id },
+      timeout: 8000,
+      headers: {
+        "X-RapidAPI-Host": "booking-com.p.rapidapi.com",
+        "X-RapidAPI-Key": "d1c9714594msh22b6c163e2c4be6p129e59jsnc056ede1f158",
+      },
+    });
+    if (res.status == 200) {
+      // test for status you want, etc
+      console.log(res.status);
+    }
+    // Don't forget to return something
+    return res.data;
+  } catch (err) {
+    console.error(err);
   }
 }
 
-
 async function consultaDePuntajes(hotelesIds) {
   let puntajesDeHoteles = [];
-    // QUERY2
-    hotelesIds.forEach((id) => {      
-      puntajesDeHoteles.push(consultaUnHotel(id).then(res => res));
-      
-  });
+  let resultado;
 
+  for (let i = 0; i < hotelesIds.length; i++) {
+    resultado = await new Promise((resolve, reject) => {
+      setTimeout(function () {
+        resolve(consultaUnHotel(hotelesIds[i]));
+      }, 1000);
+    });
+    puntajesDeHoteles.push(resultado);
+  }
   return puntajesDeHoteles;
 }
 
 router.get("/hoteles", async function (req, res, next) {
- 
-
-  // let hotelesIds = await consultaDeHoteles();
-  let mockIds = [
-    8051135, 5931564,  291801
-  ]
-  let respuesta = await consultaDePuntajes(mockIds);
+  let hotelesIds = await consultaDeHoteles();
+  // let mockIds = [8051135, 5931564, 291801, 302053, 1522452, 1093286];
+  let respuesta = await consultaDePuntajes(hotelesIds);
 
   Promise.all(respuesta).then((values) => {
     res.json(values);
   });
-
-
-
 });
 
 module.exports = router;
