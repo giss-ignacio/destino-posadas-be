65c177eccc0c5107a70641e9875244296856eba9
dario <dpiazza@gmail.com>
2022-06-18 20:33:50 -0300

    correcion prueba

--- suscripmysql.js
+++ suscripmysql.js
@@ -2,15 +2,12 @@ var mysql = require('mysql');
 //var parsedDate = new Date(Date.parse(dbDate));
 //var timepo = new Date(parsedDate.getTime() + (1000 * seconds));
 var post ={
-  time : '2022-06-18 18:06:17',
+  time : "2022-06-18 18:06:17",
   hotel_ID : "2",
-  fuente : 'Booking',
+  fuente : "Booking",
   valoracion : 5,
   clean : 5,
 }
-
-
-time.getDate()
 
 var con = mysql.createConnection({
     host: "localhost",
@@ -23,7 +20,8 @@ var con = mysql.createConnection({
   con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    var sql = "INSERT INTO Valoraciones (time, Hotel_ID, WIFI, Fuente, Valoracion, Clean) VALUES (%,%,%,%,%,%)"%(post.time, post.hotel_ID, post.WIFI, post.fuente, post.valoracion, post.clean);
+    var valores = [post.time,+",",post.hotel_ID+",", post.fuente+",", post.valoracion+",", post.clean+","].toString;
+    var sql = "INSERT INTO Valoraciones (time, Hotel_ID, WIFI, Fuente, Valoracion, Clean) VALUES (%s,%s,%s,%s,%s,%s)"%(post.time, post.hotel_ID, post.WIFI, post.fuente, post.valoracion, post.clean);
     con.query(sql, function (err, result) {
       if (err) throw err;
       console.log("1 record inserted");
