76fb6cc9915d917e2edeeef82205f82e7c30e41b
dario <dpiazza@gmail.com>
2022-06-19 20:33:46 -0300

    update query sql

--- temp/hoteles.sql
+++ temp/hoteles.sql
@@ -1,39 +1,41 @@
 CREATE TABLE `Hoteles` (
-	`id` INT NOT NULL,
-	`hotel_id` INT NOT NULL,
-	`Nombre` VARCHAR(50) NOT NULL DEFAULT '0',
-	`Direccion` VARCHAR(50) NOT NULL DEFAULT '',
-	`Ciudad` VARCHAR(50) NOT NULL DEFAULT '',
-	`Tipo` VARCHAR(50) NULL DEFAULT '',
-	`review_nr` INT NULL DEFAULT 0,
-	`id_zona` INT NOT NULL DEFAULT 0,
-	PRIMARY KEY (`id`)
-)
-;
-ALTER TABLE posadas.Hoteles MODIFY COLUMN id int(11) auto_increment NOT NULL;
-
-
-
-
-
-CREATE TABLE `Valoreciones` (
-	`id` INT NOT NULL,
-	`time` VARCHAR(50) NOT NULL DEFAULT '',
-	`Hotel_ID` INT NOT NULL DEFAULT 0,
-	`WIFI` FLOAT NOT NULL DEFAULT 0,
-	`Ubicacion` VARCHAR(50) NOT NULL DEFAULT '0',
-	`Total` FLOAT NOT NULL DEFAULT 0,
-	`Clean` FLOAT NOT NULL DEFAULT 0,
-	`Money` VARCHAR(50) NOT NULL DEFAULT '0',
-	`Personal` FLOAT NOT NULL DEFAULT 0,
-	`Confort` FLOAT NOT NULL DEFAULT 0,
-	`Instalaciones` FLOAT NOT NULL DEFAULT 0,
-	`Servicios` FLOAT NOT NULL DEFAULT 0,
-	`Calificacion` FLOAT NOT NULL DEFAULT 0,
-	PRIMARY KEY (`id`)
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`hotel_id` INT(11) NOT NULL,
+	`Nombre` VARCHAR(50) NOT NULL DEFAULT '0' COLLATE 'utf8mb4_general_ci',
+	`Direccion` VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
+	`Ciudad` VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
+	`Tipo` VARCHAR(50) NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
+	`review_nr` INT(11) NULL DEFAULT '0',
+	`id_zona` INT(11) NOT NULL DEFAULT '0',
+	PRIMARY KEY (`id`) USING BTREE,
+	UNIQUE INDEX `hotel_id` (`hotel_id`) USING BTREE
 )
 COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=6
 ;
 
-ALTER TABLE posadas.Valoreciones MODIFY COLUMN id int(11) auto_increment NOT NULL;
-ALTER TABLE posadas.Valoreciones ADD CONSTRAINT Valoreciones_FK FOREIGN KEY (Hotel_ID) REFERENCES posadas.Hoteles(id);
+
+
+
+
+
+CREATE TABLE `Valoraciones` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`time` VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
+	`Hotel_ID` INT(11) NOT NULL DEFAULT '0',
+	`WIFI` FLOAT NOT NULL DEFAULT '0',
+	`Ubicacion` VARCHAR(50) NOT NULL DEFAULT '0' COLLATE 'utf8mb4_general_ci',
+	`Total` FLOAT NOT NULL DEFAULT '0',
+	`Clean` FLOAT NOT NULL DEFAULT '0',
+	`Money` VARCHAR(50) NOT NULL DEFAULT '0' COLLATE 'utf8mb4_general_ci',
+	`Personal` FLOAT NOT NULL DEFAULT '0',
+	`Confort` FLOAT NOT NULL DEFAULT '0',
+	`Instalaciones` FLOAT NOT NULL DEFAULT '0',
+	`Servicios` FLOAT NOT NULL DEFAULT '0',
+	`Calificacion` FLOAT NOT NULL DEFAULT '0',
+	PRIMARY KEY (`id`) USING BTREE
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+;
