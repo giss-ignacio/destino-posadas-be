d2a35828c7b31777ecfa24115e23117f9b6e4ad9
dario <dpiazza@gmail.com>
2022-06-19 17:33:13 -0300

    Genero Script de Cracion de Base Tabla Hoteles

---
+++ temp/hoteles.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `Hoteles` (
+	`id` INT NOT NULL,
+	`hotel_id` INT NOT NULL,
+	`Nombre` VARCHAR(50) NOT NULL DEFAULT '0',
+	`Direccion` VARCHAR(50) NOT NULL DEFAULT '',
+	`Ciudad` VARCHAR(50) NOT NULL DEFAULT '',
+	`Tipo` VARCHAR(50) NULL DEFAULT '',
+	`review_nr` INT NULL DEFAULT 0,
+	`id_zona` INT NOT NULL DEFAULT 0,
+	PRIMARY KEY (`id`)
+)
+;
