043c53316c187f440de1d8f3d1362e4f2f65062e
dario <dpiazza@gmail.com>
2022-06-19 18:21:21 -0300

    Agrego Query Create Table Valoraciones

--- temp/hoteles.sql
+++ temp/hoteles.sql
@@ -11,3 +11,29 @@ CREATE TABLE `Hoteles` (
 )
 ;
 ALTER TABLE posadas.Hoteles MODIFY COLUMN id int(11) auto_increment NOT NULL;
+
+
+
+
+
+CREATE TABLE `Valoreciones` (
+	`id` INT NOT NULL,
+	`time` VARCHAR(50) NOT NULL DEFAULT '',
+	`Hotel_ID` INT NOT NULL DEFAULT 0,
+	`WIFI` FLOAT NOT NULL DEFAULT 0,
+	`Ubicacion` VARCHAR(50) NOT NULL DEFAULT '0',
+	`Total` FLOAT NOT NULL DEFAULT 0,
+	`Clean` FLOAT NOT NULL DEFAULT 0,
+	`Money` VARCHAR(50) NOT NULL DEFAULT '0',
+	`Personal` FLOAT NOT NULL DEFAULT 0,
+	`Confort` FLOAT NOT NULL DEFAULT 0,
+	`Instalaciones` FLOAT NOT NULL DEFAULT 0,
+	`Servicios` FLOAT NOT NULL DEFAULT 0,
+	`Calificacion` FLOAT NOT NULL DEFAULT 0,
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8mb4_general_ci'
+;
+
+ALTER TABLE posadas.Valoreciones MODIFY COLUMN id int(11) auto_increment NOT NULL;
+ALTER TABLE posadas.Valoreciones ADD CONSTRAINT Valoreciones_FK FOREIGN KEY (Hotel_ID) REFERENCES posadas.Hoteles(id);
