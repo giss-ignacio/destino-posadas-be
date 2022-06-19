755c056da7d461b0841c1d4a65180a16e5cb7892
dario <dpiazza@gmail.com>
2022-06-19 16:33:55 -0300

    update mysql query

--- suscripmysql.js
+++ suscripmysql.js
@@ -21,9 +21,11 @@ var con = mysql.createConnection({
     if (err) throw err;
     console.log("Connected!");
     var valores = [post.time,+",",post.hotel_ID+",", post.fuente+",", post.valoracion+",", post.clean+","].toString;
-    var sql = "INSERT INTO Valoraciones (time, Hotel_ID, WIFI, Fuente, Valoracion, Clean) VALUES (%s,%s,%s,%s,%s,%s)"%(post.time, post.hotel_ID, post.WIFI, post.fuente, post.valoracion, post.clean);
+    var sql = "INSERT INTO Valoraciones (time, Hotel_ID, WIFI, Fuente, Valoracion, Clean) VALUES ('2022-06-18 18:06:17',2,'Booking',5,5,7)";
     con.query(sql, function (err, result) {
       if (err) throw err;
       console.log("1 record inserted");
     });
   });
+
+  //var sql = "INSERT INTO Valoraciones (time, Hotel_ID, WIFI, Fuente, Valoracion, Clean) VALUES (%s,%s,%s,%s,%s,%s)"%(post.time, post.hotel_ID, post.WIFI, post.fuente, post.valoracion, post.clean);
