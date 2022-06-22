4bf4996b76d52596d51b84c807a41e9a2376908c
Joaquin Soriano <37905853@comunidadort.edu.ar>
2022-06-22 19:45:58 -0300

    3mejoreshoteles

--- routes/dataFrontend.js
+++ routes/dataFrontend.js
@@ -4,13 +4,35 @@ const frontendRouter = express.Router();
 require("dotenv").config();
 const getData = require("./getData.js");
 
-
 frontendRouter.get("/hoteles", async function (req, res, next) {
-    let respuesta = await getData();
+    let respuesta = await getData.getData();
 
     Promise.all([respuesta]).then((values) => {
       res.json(values);
     });
   });
+
+
+  frontendRouter.get("/top3hoteles", async function (req, res, next) {
+    let respuesta = await getData.getTop3();
+
+    var arrayDeHoteles = [];
+
+    let dato1hotel = await getData.get1Hotel(respuesta[0].Nombre)
+    let dato2hotel = await getData.get1Hotel(respuesta[1].Nombre)
+    let dato3hotel = await getData.get1Hotel(respuesta[2].Nombre)
+
+    arrayDeHoteles.push(dato1hotel,dato2hotel,dato3hotel)
+
+  //   respuesta.forEach(async function(hotel, index) {
+  //     let dato1hotel = await getData.get1Hotel(hotel.Nombre);
+  //     console.log(`${index} : ${hotel.Nombre}`);
+  //     arrayDeHoteles.push(dato1hotel);
+  // });
+
+    Promise.all([arrayDeHoteles]).then((values) => {
+      res.json(values);
+    });
+      });
 
   module.exports = frontendRouter; 
--- routes/getData.js
+++ routes/getData.js
@@ -3,7 +3,7 @@ var axios = require("axios");
 async function getData() {
   try {
     let res = await axios({
-      url: "http://localhost:1026/v2/entities/",
+      url: "http://localhost:1026/v2/entities?limit=1000",
       method: "get",
       headers: {
         Accept: "application/json",
@@ -20,4 +20,50 @@ async function getData() {
   }
 }
 
-module.exports = getData;
+async function getTop3() {
+  try {
+    let res = await axios({
+      url: "http://localhost:1026/v2/entities?q=Mes==Febrero;Concepto==Puntuacion&orderBy=!Valor&limit=3&options=keyValues&attrs=Nombre",
+      method: "get",
+      headers: {
+        Accept: "application/json",
+      },
+    });
+
+    console.log(res.data)
+
+    if (res.status == 200) {
+      // test for status you want, etc
+      console.log(res.status);
+    }
+    // Don't forget to return something
+    return res.data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function get1Hotel(nombre) {
+  try {
+    let res = await axios({
+      url: 'http://localhost:1026/v2/entities?q=Mes==Febrero;Nombre=='+nombre,
+      method: "get",
+      headers: {
+        Accept: "application/json",
+      },
+    });
+
+
+    if (res.status == 200) {
+      // test for status you want, etc
+      console.log(res.status);
+    }
+    // Don't forget to return something
+    return res.data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+
+module.exports = {getData, getTop3, get1Hotel};
