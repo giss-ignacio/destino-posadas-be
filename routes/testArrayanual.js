f27426d6210a6efcc69835f21470411a6eefba25
dario <dpiazza@gmail.com>
2022-06-24 18:35:10 -0300

    Apis para Estaditicas Valoraciones

--- routes/testArrayanual.js
+++ routes/testArrayanual.js
@@ -1,82 +1,83 @@
 const getData = require("./getData.js");
-// import {
-//     ChartComponent,
-//     SeriesCollectionDirective,
-//     SeriesDirective,
-//     Inject,
-//     DateTime,
-//     SplineAreaSeries,
-//     Legend,
-//   } from '@syncfusion/ej2-react-charts';
-  
-//   import { React,useEffect, useState } from "react";
+const getPromedioCxTipo = require("./getPromedioCxTipo.js");
+var axios = require("axios");
+const { response } = require("express");
+// const dayjs = require('dayjs');
+// let today = dayjs();
+ // console.log(today.format());
 
-// const RRSS = () => {
-//   const [valor, setValor] = useState([]);
+var ano = 2022
+var mes = "Febrero"
+var concepto = "WiFi"
+var tipo ="Hotel"
+const d_t = new Date();
+ 
+let year = d_t.getFullYear();
+let month = d_t.getMonth();
+let day = d_t.getDate();
+// let hour = d_t.getHours();
+// let minute = d_t.getMinutes();
+var tabla =[
+    ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"],
+    ["Hotel","Apart Hotel","Hostería","Residencial"]
+]
+var tablaMes =[
+    {mes:"Enero"},{mes:"Febrero"},{mes:"Marzo"},{mes:"Abril"},{mes:"Mayo"},{mes:"Junio"},{mes:"Julio"},{mes:"Agosto"},{mes:"Septiembre"},{mes:"Octubre"},{mes:"Noviembre"},{mes:"Diciembre"}
+  ]
+//  let miPromedio = await getData.getPromedioCxTipo(ano,mes,concepto,tipo);
+// console.log("mi promedio :"+miPromedio);
 
-// useEffect(() => {
-//   obtenerDatos();
-// }, []);
-
-// const obtenerDatos = async () => {
-//   const data = await fetch("http://localhost:3009/api/fedata/getServicioPorMes?a=2022&m=Enero&c=WiFi&t=Hotel'");
-//   const data2 = await data.json();
-//   setValor(data2[0])
-// };
-//console.log(await getData.getServicioPorMes(2022,"Enero","WiFi"));
-
-///*******************  Busqueda de Data INICIO***********************************
-// let Servicios = async () => {
-// await getData.getServicioPorMes(2022,"Enero","WiFi");
-// console.log(Servicios);
-// };
-
-///*******************  Busqueda de Data FIN ***********************************
-console.log(Servicios);
-var respuesta =
-[
-    [
-    //Verde  Hoteles
-    { x: new Date(2022, 1, 1), y: 2.2 },
-    { x: new Date(2022, 2, 1), y: 3.4 },
-    { x: new Date(2022, 3, 1), y: 2.8 },
-    { x: new Date(2022, 4, 1), y: 1.6 },
-    { x: new Date(2022, 5, 1), y: 2.3 },
-    { x: new Date(2022, 6, 1), y: 2.5 },
-    { x: new Date(2022, 7, 1), y: 2.9 },
-    { x: new Date(2022, 8, 1), y: 3.8 },
-    { x: new Date(2022, 9, 1), y: 1.4 },
-    { x: new Date(2022, 10, 1), y: 3.1 },
-  ],
-  [
-    //Gris
-    { x: new Date(2022, 1, 1), y: 2 },
-    { x: new Date(2022, 2, 1), y: 1.7 },
-    { x: new Date(2022, 3, 1), y: 1.8 },
-    { x: new Date(2022, 4, 1), y: 2.1 },
-    { x: new Date(2022, 5, 1), y: 2.3 },
-    { x: new Date(2022, 6, 1), y: 1.7 },
-    { x: new Date(2022, 7, 1), y: 1.5 },
-    { x: new Date(2022, 8, 1), y: 2.8 },
-    { x: new Date(2022, 9, 1), y: 1.5 },
-    { x: new Date(2022, 10, 1), y: 2.3 },
-  ],
-  [
-    //azul
-    { x: new Date(2022, 1, 1), y: 0.8 },
-    { x: new Date(2022, 2, 1), y: 1.3 },
-    { x: new Date(2022, 3, 1), y: 1.1 },
-    { x: new Date(2022, 4, 1), y: 1.6 },
-    { x: new Date(2022, 5, 1), y: 2 },
-    { x: new Date(2022, 6, 1), y: 1.7 },
-    { x: new Date(2022, 7, 1), y: 2.3 },
-    { x: new Date(2022, 8, 1), y: 2.7 },
-    { x: new Date(2022, 9, 1), y: 1.1 },
-    { x: new Date(2022, 10, 1), y: 2.3 },
-  ],
-];
+var arr = Array.from(Array(4), () => new Array(12));
+ for (var i = 0; i < 4; i++) {
+      for (var z = 0; z < 12; z++) {
+        h=d_t.getFullYear()
+        j=d_t.getMonth()
+        arr[i][z] = {x:""+h+","+(z+1)+",1",y:z};
+      }   
+   }
+console.log(tablaMes[0].mes);
+//console.log("arr");
+//console.log(arr);
+console.log(year);
+console.log(month);
+console.log(day);
 
 
 
 
-console.log(respuesta);
+//  Promise.all([miPromedio]).then((values) => {
+//     console.log("mi promedio :"+miPromedio);
+
+//     var arr = Array.from(Array(4), () => new Array(12));
+//      for (var i = 0; i < 4; i++) {
+//           for (var z = 0; z < 12; z++) {
+//             h=d_t.getFullYear()
+//             j=d_t.getMonth()
+//             arr[i][z] = {x:""+h+","+(z+1)+",1",y:z};
+//           }   
+//        }
+//        console.log("arr");
+// console.log(arr);
+// console.log(year);
+// console.log(month);
+// console.log(day);
+//   });
+
+
+//  let miPromedio = getData.getPromedioCxTipo(ano,mes,concepto,tipo);
+// console.log("mi promedio :"+miPromedio);
+
+// var arr = Array.from(Array(4), () => new Array(12));
+//  for (var i = 0; i < 4; i++) {
+//       for (var z = 0; z < 12; z++) {
+//         h=d_t.getFullYear()
+//         j=d_t.getMonth()
+//         arr[i][z] = {x:""+h+","+(z+1)+",1",y:z};
+//       }   
+//    }
+
+// console.log("arr");
+// console.log(arr);
+// console.log(year);
+// console.log(month);
+// console.log(day);
